402074f112c39b51d47f1dca0dda2aa75a9769cf
rana Aditya <ranarajput9548@gmail.com>
2024-10-21 02:47:54 +0530

    get all products method added

--- src/main/java/com/microservice/productservice/controller/ProductController.java
+++ src/main/java/com/microservice/productservice/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.microservice.productservice.controller;
 
 import com.microservice.productservice.dto.ProductRequest;
+import com.microservice.productservice.dto.ProductResponse;
 import com.microservice.productservice.model.Product;
 import com.microservice.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -16,8 +19,15 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest){
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
         return productService.createProduct(productRequest);
 
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/microservice/productservice/service/ProductService.java
+++ src/main/java/com/microservice/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.microservice.productservice.service;
 
 import com.microservice.productservice.dto.ProductRequest;
+import com.microservice.productservice.dto.ProductResponse;
 import com.microservice.productservice.model.Product;
 import com.microservice.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +17,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public Product createProduct(ProductRequest productRequest){
+    public ProductResponse createProduct(ProductRequest productRequest){
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
@@ -22,7 +25,17 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("product created");
-        return product;
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(),
+                product.getPrice());
 
+    }
+
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(),
+                        product.getPrice()))
+                .toList();
     }
 }
